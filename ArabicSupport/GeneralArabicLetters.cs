47e628be6fe68b35ef56ec74b99925c552bfc56e
7yPh00N <typhoon0001@qq.com>
2025-02-18 21:35:30 +0800

    Add files via upload

---
+++ ArabicSupport/GeneralArabicLetters.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+// Token: 0x02000004 RID: 4
+internal enum GeneralArabicLetters
+{
+	// Token: 0x0400002B RID: 43
+	Hamza = 1569,
+	// Token: 0x0400002C RID: 44
+	Alef = 1575,
+	// Token: 0x0400002D RID: 45
+	AlefHamza = 1571,
+	// Token: 0x0400002E RID: 46
+	WawHamza,
+	// Token: 0x0400002F RID: 47
+	AlefMaksoor,
+	// Token: 0x04000030 RID: 48
+	AlefMagsora = 1609,
+	// Token: 0x04000031 RID: 49
+	HamzaNabera = 1574,
+	// Token: 0x04000032 RID: 50
+	Ba = 1576,
+	// Token: 0x04000033 RID: 51
+	Ta = 1578,
+	// Token: 0x04000034 RID: 52
+	Tha2,
+	// Token: 0x04000035 RID: 53
+	Jeem,
+	// Token: 0x04000036 RID: 54
+	H7aa,
+	// Token: 0x04000037 RID: 55
+	Khaa2,
+	// Token: 0x04000038 RID: 56
+	Dal,
+	// Token: 0x04000039 RID: 57
+	Thal,
+	// Token: 0x0400003A RID: 58
+	Ra2,
+	// Token: 0x0400003B RID: 59
+	Zeen,
+	// Token: 0x0400003C RID: 60
+	Seen,
+	// Token: 0x0400003D RID: 61
+	Sheen,
+	// Token: 0x0400003E RID: 62
+	S9a,
+	// Token: 0x0400003F RID: 63
+	Dha,
+	// Token: 0x04000040 RID: 64
+	T6a,
+	// Token: 0x04000041 RID: 65
+	T6ha,
+	// Token: 0x04000042 RID: 66
+	Ain,
+	// Token: 0x04000043 RID: 67
+	Gain,
+	// Token: 0x04000044 RID: 68
+	Fa = 1601,
+	// Token: 0x04000045 RID: 69
+	Gaf,
+	// Token: 0x04000046 RID: 70
+	Kaf,
+	// Token: 0x04000047 RID: 71
+	Lam,
+	// Token: 0x04000048 RID: 72
+	Meem,
+	// Token: 0x04000049 RID: 73
+	Noon,
+	// Token: 0x0400004A RID: 74
+	Ha,
+	// Token: 0x0400004B RID: 75
+	Waw,
+	// Token: 0x0400004C RID: 76
+	Ya = 1610,
+	// Token: 0x0400004D RID: 77
+	AlefMad = 1570,
+	// Token: 0x0400004E RID: 78
+	TaMarboota = 1577,
+	// Token: 0x0400004F RID: 79
+	PersianPe = 1662,
+	// Token: 0x04000050 RID: 80
+	PersianChe = 1670,
+	// Token: 0x04000051 RID: 81
+	PersianZe = 1688,
+	// Token: 0x04000052 RID: 82
+	PersianGaf = 1711
+}
